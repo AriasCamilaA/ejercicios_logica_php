3a50fd5f54b2ca9d91f65acd0c5c429bc1fa3846
AriasCamilaA <122493418+AriasCamilaA@users.noreply.github.com>
2023-04-21 13:54:30 -0500

    Update 08-promedio.php

--- 01-variables/08-promedio.php
+++ 01-variables/08-promedio.php
@@ -1,5 +1,4 @@
 <?php
-# Programa para calcular el área de un cuadrado, la longitud de un lado la ingresa el usuario
 $cont = 0;
 for ($i = 1; $i <= 5; $i++) {
     $cont += (int) readline("Ingrese el número $i: ");
