5de2193a45403d6979df96aca14289a4092bc000
AriasCamilaA <122493418+AriasCamilaA@users.noreply.github.com>
2023-04-21 13:10:46 -0500

    Update 11-raiz.php

--- 01-variables/11-raiz.php
+++ 01-variables/11-raiz.php
@@ -3,26 +3,8 @@
 # permita calcular la raíz cuadrada del mismo (sin usar función)
 
 $n = (int)readline("Ingrese el número al que le quiere calcular la raiz: ");
-$i = $r = 0;
-while ($r <= $n) {
-    $i++;
-    $r = $i ** 2;
-}
-$i--;
-$r = $i ** 2;
 
-if ($i ** 2 != $n) {
-    for ($f = 0; $f < 8; $f++) {
-        $sum = 1 / (10 ** $f);
-        while ($r <= $n) {
-            $i += $sum;
-            $r = $i ** 2;
-        }
-        $i -= $sum;
-        $r = $i ** 2;
-    }
-}
-
-echo "La raiz de " . $n . " es " . $i . " ---->" . sqrt($n);
+$rta = $n**(1/2);
+echo "La raiz de $n es $rta" ;
 
 ?>
